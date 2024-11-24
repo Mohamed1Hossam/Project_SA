6becee3705f2417700e06519a3c468b5fd52ce90
Mohamed Hossam <mohamed.hmg16@gmail.com>
2024-11-24 02:30:29 +0200

    .

--- Login/AutoNova Sign up.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-   event.preventDefault(); // Prevent default form submission
-   // You can add your form submission logic here
-});
